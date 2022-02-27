eec0b4f749bf62375223fd4bad8fc680943f84dd
Jordan Mata <jmata9370@sdsu.edu>
2022-02-27 12:56:31 -0800

    Cosmetic newline after each pi printout.

--- src/MonteCarloApproximation.cpp
+++ src/MonteCarloApproximation.cpp
@@ -31,7 +31,7 @@ int main(int argc, char** argv){
 
   MonteCarloApproximation mca = MonteCarloApproximation(args->numThreads, args->numPoints);
   std::cout << "Using " << args->numThreads << " threads and " << args->numPoints << " random points:\n";
-  std::cout << "Pi: " << mca.runApproximation();
+  std::cout << "Pi: " << mca.runApproximation() << std::endl;
 
   return 0;
 }
