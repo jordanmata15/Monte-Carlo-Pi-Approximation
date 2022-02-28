f38da77f1d2871310c51b99f1835c9f7ade12800
Jordan Mata <jmata9370@sdsu.edu>
2022-02-28 10:38:20 -0800

    Corrected script to plot to the data folder.

--- scripting/plot_data.py
+++ scripting/plot_data.py
@@ -6,14 +6,17 @@ import matplotlib.pyplot as plt
 import os
 import sys
 
-# data directory relative to the "scripting" folder
-# assumes we run the script while in the scripting folder
-data_filename="../data/times.csv"
+path_to_script = os.path.dirname(__file__)
+
+data_path = os.path.join(path_to_script, "../data")
+data_filename = os.path.join(data_path, "times.csv") # data folder relative to this script
+file_full_path = os.path.join(path_to_script, data_filename)
 
 
 def crunch_data(filename):
     # num_threads, num_random_points, pi_approximation, time_elapsed
     df = pd.read_csv(filename)
+    print(df)
     plot_threads_to_time(df[['num_threads','num_random_points', 'time_elapsed']])
 
 
@@ -28,19 +31,20 @@ def plot_threads_to_time(times_df):
     x_labels=[str(i) for i in x]
     y_labels=[i+1 for i in range(int(max(y))+1)]
 
-    fix, ax = plt.subplots(1,1)
+    fig, ax = plt.subplots(1,1)
     ax.set_ylim(ymin=0)
     ax.set_yticks(y_labels)
     ax.set_yticklabels(y_labels)
     plt.xlabel('Number of Threads')
     plt.ylabel('Time Elapsed (seconds)')
     plt.plot(x_labels, y)
-    matplotlib.pyplot.show()
-    #fig.savefig("plot.pdf")
+    #matplotlib.pyplot.show()
+    fig.savefig(os.path.join(data_path, "plot.pdf"))
 
 
 if __name__=="__main__":
     if len(sys.argv) > 1:
         data_filename = sys.argv[1]
 
-    crunch_data(data_filename)
+    #print()
+    crunch_data(file_full_path)
