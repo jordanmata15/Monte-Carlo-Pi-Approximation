cea16e121df905406178013ec82c5c1a261cee5d
Jordan Mata <jmata9370@sdsu.edu>
2022-03-05 14:06:02 -0800

    Seeded random with a time instead of constant seed.

--- src/MonteCarloApproximation.cpp
+++ src/MonteCarloApproximation.cpp
@@ -12,15 +12,14 @@ double MonteCarloApproximation::runApproximation(){
   #pragma omp parallel for private(x,y) reduction(+:innerCount) num_threads(this->numThreads)
   for (int i=0; i<this->pointsToPlot; ++i){
     // https://stackoverflow.com/questions/29709897/c-thread-safe-uniform-distribution-random-number-generation
-    int thread_num = omp_get_thread_num();
-    thread_local std::mt19937 generator(thread_num); // generator(time(0));
+    thread_local std::mt19937 generator(time(0));
     std::uniform_real_distribution<> uniform_dist(0, this->radius);
 
     x = uniform_dist(generator);
     y = uniform_dist(generator);
     if (this->isWithinUnitDistance(x,y)) ++innerCount;
   }
-  
+
   gettimeofday(&endTime, NULL);
   timersub(&endTime, &startTime, &elapsedTime);
   timeManager->recordTime(&elapsedTime);
