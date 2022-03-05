18c14ef7fddb62a5ac094e7d0e1306546cbbcce0
Jordan Mata <jmata9370@sdsu.edu>
2022-03-05 14:19:37 -0800

    Corrected documentation. Removed unused imports.

--- src/ArgParser.hpp
+++ src/ArgParser.hpp
@@ -2,7 +2,6 @@
 #define ARG_PARSER_HPP
 
 #include <iostream>
-#include <string>
 #include <getopt.h>
 
 #define DEFAULT_THREADS 1
@@ -12,7 +11,8 @@
               "\t-p optional flag that reads in NUMPOINTS (NUMPOINTS > 0, integer)\n"
 
 /**
- * Arguments object to hold the values of the desired inputs given by the user.
+ * @brief Arguments object to hold the values of the desired inputs given by the user.
+ * 
  */
 class Arguments{
   public:
@@ -20,7 +20,8 @@ class Arguments{
     int numPoints;
 
     /**
-     * Basic constructor. Sets default values.
+     * @brief Construct a new Arguments object with default values.
+     * 
      */
     Arguments():numThreads(DEFAULT_THREADS), 
                 numPoints(DEFAULT_POINTS){}
@@ -28,36 +29,48 @@ class Arguments{
 
 
 /**
- * Class to continuously prompt the user for input and validate it. Fills an 
- * Arguments object with the users desired input.
+ * @brief Class to read in command line arguments and validate them. Fills an Arguments 
+ *        object with the users desired input.
  */
 class ArgParser {
 
   private:
+    /**
+     * @brief Reads in an string and returns the converted integer.
+     * 
+     * @param value String value to convert to an integer. 
+     * @return int The converted integer. Returns -1 if the string was not successfully converted.
+     */
     int readInt(char flag, char* value);
 
+    /**
+     * @brief Prints out the usage of our program.
+     * 
+     */
     void printUsage(){ std::cout << USAGE << std::endl; }
   
   public:
     Arguments* args;
 
     /**
-     * Simple constructor that dynamically allocates memory for the arguments.
+     * @brief Construct a new Arg Parser object. Dynamically allocate the memory.
+     * 
      */
     ArgParser() { args = new Arguments(); }
 
     /**
-     * Simple destructor that frees the arguments once we're done with them.
+     * @brief Destroy the Arg Parser object and free the allocated memory.
+     * 
      */
     ~ArgParser(){ delete args; }
 
     /**
-     * Method used to parse the flags from the user. Validates integer arguments
+     * @brief Method used to parse the flags from the user. Validates integer arguments
      * and exits if invalid.
      * 
      * @param argc The argument count provided to main().
      * @param argv The argument list provided to main().
-     * @return A pointer to an argument structure from which the user inputs
+     * @return Arguments* A pointer to an argument structure from which the user inputs
      *         can be read.
      */
     Arguments* parseArgs(int argc, char** argv);
