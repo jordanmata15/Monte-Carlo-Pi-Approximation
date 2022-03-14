7c7d211a9f45662870c69174a49ffd0a26426c8f
Jordan Mata <jmata9370@sdsu.edu>
2022-03-14 16:10:49 -0700

    Made each graph it's own plot.

--- scripting/plot_data.py
+++ scripting/plot_data.py
@@ -25,36 +25,24 @@ def crunch_data(filename):
 Plot number of threads to the time elapsed. One line for each data size. 
 """
 def plot_threads_to_time(times_df):
-    max_points_mask = times_df["num_random_points"]==max(times_df["num_random_points"])
-    df = times_df[max_points_mask][['num_threads', 'time_elapsed']]
-
-    # get x/y axis labels
-    x = df['num_threads']
-    y = df['time_elapsed']
-    x_labels=[format(i, ",") for i in x]
-    y_labels=[i+1 for i in range(int(max(y))+1)]
+    sizes = times_df["num_random_points"].unique()
 
     # format the plot
-    fig, ax = plt.subplots(1,1)
-    ax.set_ylim(ymin=0)
-    ax.set_yticks(y_labels)
-    ax.set_yticklabels(y_labels)
-    plt.xlabel('Number of Threads')
-    plt.ylabel('Time Elapsed (seconds)')
-
-    # plot over all counts of random points
-    for points in times_df['num_random_points'].unique():
-        points_df = times_df[times_df["num_random_points"]==int(points)]
-        #format x/y data for this number of points
-        x = points_df['num_threads']
+    fig, axs = plt.subplots(math.ceil(len(sizes)/2),2, constrained_layout=True)
+    
+    for ax in axs.flat:
+        ax.set(xlabel='Number of Threads', ylabel='Time (seconds)')
+    
+    for ax,size in zip(axs.flat, sizes):
+        local_df = times_df[times_df["num_random_points"] == size]
+        x = local_df['num_threads']
         x_labels=[str(i) for i in x]
-        y = points_df['time_elapsed']
+        y = local_df['time_elapsed']
+        ax.set_title("Random Points: " + str(size) +"^2", color="red")
+        ax.plot(x_labels, y)
 
-        plt.plot(x_labels, y, label=format(points, ","))
-        ax.legend()
-
+    fig.set_size_inches(18.5, 10.5)
     fig.savefig(os.path.join(data_path, "Plot_Threads_Time.pdf"), bbox_inches = "tight")
-
 
 
 """
@@ -84,7 +72,6 @@ def plot_points_to_accuracy(times_df):
     plt.autoscale()
     plt.plot(x_labels, y)
     plt.savefig(os.path.join(data_path, "Plot_Points_Accuracy.pdf"), bbox_inches = "tight")
-
 
 
 if __name__=="__main__":
